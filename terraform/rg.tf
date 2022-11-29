9b1efc0c7ad6b957a06a304b882c901aba5c83f3
mcsalah <macsalah@gmail.com>
2022-11-29 20:09:43 +0100

    Update rg.tf

--- terraform/rg.tf
+++ terraform/rg.tf
@@ -5,3 +5,13 @@ resource "azurerm_resource_group" "rgtftest" {
         envirennement = "dev"
   }
 }
+resource "azurerm_storage_account" "staccvtf" {
+  name                     = "sttfdevopssalah"
+  resource_group_name      = azurerm_resource_group.rgtftest.name
+  location                 = azurerm_resource_group.rgtftest.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
