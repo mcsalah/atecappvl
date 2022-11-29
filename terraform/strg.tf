9cccf5b5303e343697e68c6811a8a7c791d193fd
mcsalah <macsalah@gmail.com>
2022-11-29 20:18:13 +0100

    Create strg.tf

---
+++ terraform/strg.tf
@@ -0,0 +1,11 @@
+resource "azurerm_storage_account" "staccvtf" {
+  name                     = "sttfdevopssalah"
+  resource_group_name      = azurerm_resource_group.rgtftest.name
+  location                 = azurerm_resource_group.rgtftest.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
